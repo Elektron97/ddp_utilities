47ef01ba4a4972bba3bcf8b775f3a753d6776b18
Daniele Caradonna <danele.caradonna@gmail.com>
2025-03-18 14:54:12 +0400

    import ddp in my_ddp

--- my_ddp.m
+++ my_ddp.m
@@ -1,92 +1,214 @@
-% Modification by SoRoSim team ;)
+% Copyright (C) 2019 Maitreya Venkataswamy - All Rights Reserved
 
+% Modification by SoRoSim team
 %%%      DDP for GVS Hybrid-Kinematic Chain                   %%%
 % This code is a readaption of the code developed by            %
 % Maitreya Venkataswamy, to fit in the SoRoSim Framework.       %
 % Original code: https://github.com/maitreyakv/ddp-simulation.  %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% The idea here is to merge the solutions that we found online, mimicking
-% also the algorithm described here:
-% https://www.imgeorgiev.com/2023-02-01-ddp/.
-% At the same time, we want to keep the structure of the optimTraj package,
-% since we're working on the comparison on the two methods.
+%% Differential Dynamic Programming Algorithm Function
 
-function sol = my_ddp(dyn_obj, cost_obj, x0, tf, N)
-    arguments
-        dyn_obj
-        cost_obj
-        x0
-        tf
-        N
-    end
+% Performs Differential Dynamic Programming Algorithm to produce a
+% locally optimal control sequence.
+%
+% Inputs
+% 
+% x_0      : initial state vector
+% x_star   : target state vector
+% t_f      : time horizon
+% N        : number of discretizations of time
+% dyn      : instance of Dynamics class
+% cost     : instance of Cost class
+% u_max    : maximum magnitude of control, used for clamping
+% num_iter : number of DDP iterations
+% alpha    : DDP learning parameter for line searching
+%
+% Outputs
+% 
+% sol : structure with solution components
+%
+function sol = my_ddp(x_0, t_f, N, dyn, cost, u_max, num_iter, alpha)
+    %% Allocate arrays for DDP
 
-    %% Addpath to the Classes
-    addpath("classes");
-
-    %% Allocate variable
     % Time stamp array and timestep
     t = linspace(0.0, t_f, N);
     dt = t(2) - t(1);
-
+    
     % Cost history
     J = zeros(num_iter, 1);
+    
+    % Control energy history
+    E = zeros(num_iter, 1);
+    
+    % State trajectory
+    x = cell(N, 1);
+    x_new = cell(N, 1);
+    x{1} = x_0;
+    x_new{1} = x_0;
 
-    % Input
-    u = zeros(dyn_obj.nact, N);
 
-    % State Space
-    x = [x0, zeros(dyn_obj.ndof, N - 1)];
-
+    % Control Input trajectory
+    u = cell(N, 1);
+    
     % Value function and derivatives
     V = zeros(N, 1);
-
-    % % Derivatives
-    % V_x = cell(N, 1);
-    % V_xx = cell(N, 1);
+    V_x = cell(N, 1);
+    V_xx = cell(N, 1);
     
-    % % State action value derivatives
-    % Q_x = cell(N, 1);
-    % Q_u = cell(N, 1);
-    % Q_xx = cell(N, 1);
-    % Q_uu = cell(N, 1);
-    % Q_xu = cell(N, 1);
-    % Q_ux = cell(N, 1);
+    % State action value derivatives
+    Q_x = cell(N, 1);
+    Q_u = cell(N, 1);
+    Q_xx = cell(N, 1);
+    Q_uu = cell(N, 1);
+    Q_xu = cell(N, 1);
+    Q_ux = cell(N, 1);
 
-    %% Nominal Input (Random Input)
-    disp("initializing random control sequence...")
+    %% Initialize DDP with a random input sequence
+
+    fprintf("initializing random control sequence...\n")
     
-    % Random Input
-    u(:, 1:end-1) = rand(dyn_obj.nact, N - 1);
-    % Implicit: u(N) = 0;
-    disp("generating initial trajectory...")
-
-    %% Nominal Trajectory & Analytical Derivatives
-    for i = 1:(N - 1)
-        % Gradients
+    % Generate random control sequence
+    for k = 1:N-1
+        u{k} = 2 .* u_max .* rand(length(u_max), 1) - u_max;
     end
+    u{N} = zeros(numel(u_max), 1);
+    
+    fprintf("generating initial trajectory...\n")
 
+    x_dot = cell(N, 1);
+    x_dot{1} = dyn.F(t(1), x_new{1}, u{1});
+    
+    % Generate initial trajectory using random control sequence
+    for k = 1:N-1
+        % Store x_dot
+        x_dot{k + 1} = dyn.F(t(k), x_new{k}, u{k});
 
-    % % State trajectory
-    % x = cell(N, 1);
-    % x_new = cell(N, 1);
-    % x{1} = x_0;
-    % x_new{1} = x_0;
-    % 
-    % 
-    % % Control Input trajectory
-    % u = cell(N, 1);
-    % 
-    % % Value function and derivatives
-    % V = zeros(N, 1);
-    % V_x = cell(N, 1);
-    % V_xx = cell(N, 1);
-    % 
-    % % State action value derivatives
-    % Q_x = cell(N, 1);
-    % Q_u = cell(N, 1);
-    % Q_xx = cell(N, 1);
-    % Q_uu = cell(N, 1);
-    % Q_xu = cell(N, 1);
-    % Q_ux = cell(N, 1);
+        % Explicit Forward Euler
+        x_new{k+1} = x_new{k} + dyn.F(t(k), x_new{k}, u{k}) .* dt;
+    end
+    
+    %% Perform main DDP iterations on the trajectory and input sequence
+    
+    fprintf("beginning DDP...\n")
+    
+    for i = 1:num_iter
+        fprintf("DDP iteration %d out of %d...\n", i, num_iter);
+        
+        % Update control sequence from previous iteration
+        if i > 1
+            for k = 1:N-1
+                % Compute control update feed-forward and feed-back
+                du_ff = -inv(Q_uu{k}) * Q_u{k};
+                du_fb = -inv(Q_uu{k}) * Q_ux{k} * (x_new{k} - x{k});
+                
+                % Limit feed forward control modification with clamping
+                for m = 1:numel(u_max)
+                    du_ff(m) = min(u_max(m), max(-u_max(m), ...
+                                         du_ff(m) + u{k}(m))) - u{k}(m);
+                end
+                
+                % Update control
+                u{k} = u{k} + alpha .* (du_ff + du_fb);
+                
+                % Compute next state in trajectory with new control
+                x_new{k+1} = x_new{k} + dyn.F(t(k), x_new{k}, u{k}) .* dt;
+                
+                % Return error if problem with trajectory
+                if isnan(x_new{k+1})
+                    sol = assemble_solution(x, u, t, J, E, Q_u, ...
+                                            Q_uu, Q_ux, 1);
+                    return
+                end
+            end
+        end
+        
+        % Update the current trajectory
+        x = x_new;
+        
+        % Compute total cost
+        J(i) = cost.phi(x{N}, x_star);
+        for k = 1:N-1
+            J(i) = J(i) + cost.L(x{k}, u{k}, dt);
+        end
+        
+        % Compute control energy usage
+        for k = 1:N-1
+            E(i) = E(i) + 0.5 .* u{k}.' * u{k} .* dt;
+        end
+        
+        % Compute terminal value function and derivatives
+        V(N) = cost.phi(x{N}, x_star);
+        V_x{N} = cost.phi_x(x{N}, x_star);
+        V_xx{N} = cost.phi_xx(x{N}, x_star);
+        
+        % Perform backwards pass
+        for k = N-1:-1:1
+            % Compute Analytical Derivatives
+            [fx, fu] = dyn.analytical_derivatives(t(k), x{k}, x_dot{k}, u{k});
+
+            Q_x{k} = cost.L_x(x{k}, u{k}, dt) + ...
+                     fx.' * V_x{k+1};
+            Q_u{k} = cost.L_u(x{k}, u{k}, dt) + ...
+                     fu.' * V_x{k+1};
+            Q_xx{k} = cost.L_xx(x{k}, u{k}, dt) ...
+                      + fx.' * V_xx{k+1} ...
+                      * fx;
+            Q_uu{k} = cost.L_uu(x{k}, u{k}, dt) ...
+                      + fu.' * V_xx{k+1} ...
+                      * fu;
+            Q_xu{k} = cost.L_xu(x{k}, u{k}, dt) ...
+                      + fx.' * V_xx{k+1} ...
+                      * fu;
+            Q_ux{k} = cost.L_ux(x{k}, u{k}, dt) ...
+                      + fu.' * V_xx{k+1} ...
+                      * fx;
+               
+            % Compute the value function derivatives
+            V_x{k} = Q_x{k} - Q_xu{k} * (Q_uu{k} \ Q_u{k});
+            V_xx{k} = Q_xx{k} - Q_xu{k} * (Q_uu{k} \ Q_ux{k});
+        end
+    end
+    
+    %% Assemble and return solution structure
+    
+    fprintf("finished DDP, assembling results for post-processing...\n");
+    
+    % Assemble solution
+    sol = assemble_solution(x, u, t, J, E, Q_u, Q_uu, Q_ux, 0);
+end
+
+%% Helper Functions for DDP Algorithm
+
+% Assembles and returns solution structure
+%
+% Inputs
+%
+% x               : locally optimal state trajectory
+% u               : locally optimal control sequence
+% t               : discretized time stamps
+% J               : iteration history of cost function
+% E               : iteration history of control energy
+% Q_u, Q_uu, Q_ux : derivatives of state action value function
+% error           : zero if no error, nonzero else
+%
+% Outputs
+%
+% sol : solution structure
+function sol = assemble_solution(x, u, t, J, E, Q_u, Q_uu, Q_ux, error)
+
+    % Solution structure
+    sol = struct;
+    sol.error = error;
+    sol.x = x;
+    sol.u = u;
+    sol.t = t;
+    sol.dt = t(2) - t(1);
+    sol.J = J;
+    sol.E = E;
+    sol.Q_u = Q_u;
+    sol.Q_uu = Q_uu;
+    sol.Q_ux = Q_ux;
+    
+    return
 end
