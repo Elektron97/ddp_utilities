92377a638857c4306b970ee0f391c16a478b5915
Daniele Caradonna <danele.caradonna@gmail.com>
2025-04-03 14:13:14 +0400

    numerical hessian

--- classes/GVS_Dynamics.m
+++ classes/GVS_Dynamics.m
@@ -55,5 +55,55 @@ classdef GVS_Dynamics < Dynamics
             fu = fu*dt;
         end
 
+        %% Numerical Hessian
+        function [fxx, fxu, fuu] = numerical_hessian(obj, x, u, dt, options)
+            arguments
+                obj
+                x
+                u
+                dt
+                options.eps = 1e-6
+            end
+            
+            % Init Hessians
+            fxx = zeros(obj.nx, obj.nx, obj.nx);
+            fxu = zeros(obj.nx, obj.nx, obj.nact);
+            fuu = zeros(obj.nx, obj.nact, obj.nact);
+
+            % Storing Canonical Base
+            x_bases = eye(obj.nx);
+            u_bases = eye(obj.nact);
+
+            % Differentiation w.r.t. x
+            for i = 1:obj.nx
+                % Perturbating x
+                x_tilde1 = x - eps*x_bases(:, i);
+                x_tilde2 = x + eps*x_bases(:, i);
+
+                % Compute Analytical Derivatives
+                [fx1, ~] = obj.analytical_derivatives(0, x_tilde1, obj.dynamics(0, x_tilde1, u), u, dt);
+                [fx2, ~] = obj.analytical_derivatives(0, x_tilde2, obj.dynamics(0, x_tilde2, u), u, dt);
+
+                % Finite Difference
+                fxx(:, :, i) = 0.5*(fx2 - fx1)/eps;
+            end
+
+            % Differentiation w.r.t. u
+            for i = 1:obj.nact
+                % Perturbating x
+                u_tilde1 = u - eps*u_bases(:, i);
+                u_tilde2 = u + eps*u_bases(:, i);
+
+                % Compute Analytical Derivatives
+                [fx1, fu1] = obj.analytical_derivatives(0, x, obj.dynamics(0, x, u_tilde1), u_tilde1, dt);
+                [fx2, fu2] = obj.analytical_derivatives(0, x, obj.dynamics(0, x, u_tilde2), u_tilde2, dt);
+
+                % Finite Difference
+                fxu(:, :, i) = 0.5*(fx2 - fx1)/eps;
+                fuu(:, :, i) = 0.5*(fu2 - fu1)/eps;
+            end
+
+        end
+
     end
 end
