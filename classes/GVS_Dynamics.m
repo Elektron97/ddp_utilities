28cf56f0fb833d41741de6051b5dd075090c22b6
Daniele Caradonna <danele.caradonna@gmail.com>
2026-04-29 15:07:54 +0200

    central difference

--- classes/GVS_Dynamics.m
+++ classes/GVS_Dynamics.m
@@ -1,6 +1,5 @@
 %%% Class for a generic SoRoSim object, that inherits the abstract Dynamics
 %%% class to fit the DDP problem.
-
 classdef GVS_Dynamics < Dynamics
     properties
         robot_linkage       % SoRoSim Object
@@ -10,7 +9,6 @@ classdef GVS_Dynamics < Dynamics
         fsolve_opt          % Store fsolve options for Implicit Time Integration
         % odeopts             % Store ode options for Stiff ODEs
     end
-
     methods
         %% Constructor
         function obj = GVS_Dynamics(robot_linkage, options)
@@ -25,16 +23,14 @@ classdef GVS_Dynamics < Dynamics
             obj.ndof = obj.robot_linkage.ndof;
             obj.nx = 2*obj.ndof;
             obj.nact = obj.robot_linkage.nact;
-
             % Init fsolve options
             obj.fsolve_opt = optimoptions('fsolve', 'Algorithm', 'trust-region-dogleg', ...
                                             'Display', 'none', 'SpecifyObjectiveGradient', true, ...
                                             'FunctionTolerance', options.max_ftolerance, ...
                                             'MaxIterations', options.max_iteration);
-
             % obj.odeopts = odeset('Jacobian', @(t, xk) obj.analytical_derivatives(t, xk, obj.dynamics(t, xk, u), u));
         end
-
+        
         %% Dynamics
         function x_dot = dynamics(obj, t, x, u)
             % dynamicsSolver function
@@ -43,7 +39,7 @@ classdef GVS_Dynamics < Dynamics
             % dxdt = [q_dot; q_2dot]
             x_dot = [x(obj.ndof + 1:end); y(1:obj.ndof)];
         end
-
+        
         %% Discretization of Dynamics
         function x_new = discretize_dynamics(obj, t, x, u, h, options)
             arguments
@@ -55,18 +51,15 @@ classdef GVS_Dynamics < Dynamics
                 options.method = "ode1"
                 options.specify_jacobian = false
             end
-
             %% Discretization
             switch(options.method)
                 case "ode1"
-                    % Compute Continous Dynamics
+                    % Compute Continuous Dynamics
                     x_dot = obj.dynamics(t, x, u);
-
                     x_new = x + h*x_dot;
                 case "ode4"
-                    % Compute Continous Dynamics
+                    % Compute Continuous Dynamics
                     x_dot = obj.dynamics(t, x, u);
-
                     k0 = x_dot;
                     k1 = obj.dynamics(t + 0.5*h, x + 0.5*h*k0, u);
                     k2 = obj.dynamics(t + 0.5*h, x + 0.5*h*k1, u);
@@ -80,7 +73,6 @@ classdef GVS_Dynamics < Dynamics
                     x_new = YOUT(end, :)';
                 case "ode15s"
                     ODEFUN = @(t, xk) obj.dynamics(t, xk, u);
-
                     % Increase Speed
                     if options.specify_jacobian
                         odeopts = odeset('Jacobian', @(t, xk) obj.analytical_derivatives(t, xk, obj.dynamics(t, xk, u), u));
@@ -88,29 +80,24 @@ classdef GVS_Dynamics < Dynamics
                     else
                         [~, YOUT] = ode15s(ODEFUN, [t, t + h], x);
                     end
-
                     x_new = YOUT(end, :)';
                 
                 %% Implicit Time Integration
                 case "ode1i"
                     % Create Function handle of the Residual
                     ODEFUN = @(x_next) computeResidualAndJacobian(t, x_next, x, u, h, obj, "method", options.method);
-
                     % fsolve: Initial Guess x_k (continuity)
                     x_new = fsolve(ODEFUN, x, obj.fsolve_opt);  
-
                 case "trapz"
                     % Create Function handle of the Residual
                     ODEFUN = @(x_next) computeResidualAndJacobian(t, x_next, x, u, h, obj, "method", options.method);
-
                     % fsolve: Initial Guess x_k (continuity)
                     x_new = fsolve(ODEFUN, x, obj.fsolve_opt);
-
                 otherwise
                     error("Unsupported Integration Method.");
             end
         end
-
+        
         %% Analytical Derivatives (First Order)
         function [fx, fu] = analytical_derivatives(obj, t, x, xdot, u)
             q    = x(1:obj.ndof);
@@ -128,12 +115,11 @@ classdef GVS_Dynamics < Dynamics
             M = dID_dqdd;
             dFDdq  = M\(dtau_dq-dID_dq);
             dFDdqd = M\(dtau_dqd-dID_dqd);
-
             %% Gradients
             fx = [zeros(obj.ndof, obj.ndof), eye(obj.ndof); dFDdq, dFDdqd];
             fu = [zeros(obj.ndof, obj.nact); M\B];
         end
-
+        
         %% Apply Discretization
         function [fx, fu] = discretized_gradients(obj, t, x, u, h, options)
             arguments
@@ -143,52 +129,76 @@ classdef GVS_Dynamics < Dynamics
                 u
                 h
                 options.method = "ode1"
+                options.grad_method = "analytical" % "analytical" or "central_difference"
+                options.eps = 1e-6                 % Step size for finite difference
             end
-
-            %% Gradients of the Discretized System
+            
+            %% Numerical Estimation (Central Difference)
+            if options.grad_method == "central_difference"
+                fx = zeros(obj.nx, obj.nx);
+                fu = zeros(obj.nx, obj.nact);
+                
+                % Finite differences with respect to state (x)
+                for i = 1:obj.nx
+                    dx = zeros(obj.nx, 1);
+                    dx(i) = options.eps;
+                    % Evaluate discretized dynamics at (x + dx) and (x - dx)
+                    x_plus  = obj.discretize_dynamics(t, x + dx, u, h, "method", options.method);
+                    x_minus = obj.discretize_dynamics(t, x - dx, u, h, "method", options.method);
+                    % Central difference
+                    fx(:, i) = (x_plus - x_minus) / (2 * options.eps);
+                end
+                
+                % Finite differences with respect to input (u)
+                for i = 1:obj.nact
+                    du = zeros(obj.nact, 1);
+                    du(i) = options.eps;
+                    % Evaluate discretized dynamics at (u + du) and (u - du)
+                    x_plus  = obj.discretize_dynamics(t, x, u + du, h, "method", options.method);
+                    x_minus = obj.discretize_dynamics(t, x, u - du, h, "method", options.method);
+                    % Central difference
+                    fu(:, i) = (x_plus - x_minus) / (2 * options.eps);
+                end
+                
+                return; % Early exit once numerical gradients are computed
+            end
+            
+            %% Gradients of the Discretized System (Analytical)
             switch(options.method)
                 case "ode1"
-                    % Compute Analytical Derivatives (Continous System)
+                    % Compute Analytical Derivatives (Continuous System)
                     xdot = obj.dynamics(t, x, u);
                     [fx_continous, fu_continous] = obj.analytical_derivatives(t, x, xdot, u);
-
                     fx = eye(obj.nx) + fx_continous*h;
                     fu = fu_continous*h;
                 case "ode4"
-                    % Compute Analytical Derivatives (Continous System)
+                    % Compute Analytical Derivatives (Continuous System)
                     xdot = obj.dynamics(t, x, u);
                     [fx_continous, fu_continous] = obj.analytical_derivatives(t, x, xdot, u);
-
-                    k0 = xdot;  % xdot = f(x, u) | Continous
+                    k0 = xdot;  % xdot = f(x, u) | Continuous
                     k1 = obj.dynamics(t + 0.5*h, x + 0.5*h*k0, u);
                     k2 = obj.dynamics(t + 0.5*h, x + 0.5*h*k1, u);
                     k3 = obj.dynamics(t + h, x + h*k2, u);
-
                     % Compute Analytical Derivatives of i_th k
                     % Init
                     k0x = fx_continous;
                     k0u = fu_continous;
-
                     % k-ith
                     [k1x, k1u] = obj.analytical_derivatives(t + 0.5*h, x + 0.5*h*k0, k1, u);
                     [k2x, k2u] = obj.analytical_derivatives(t + h/2, x + 0.5*h*k1, k2, u);
                     [k3x, k3u] = obj.analytical_derivatives(t + h, x + h*k2, k3, u);
-
                     % Chain Rule Correction (Input) | diff_u(x_k + 0.5*h*k0(u))
                     k1u = k1u + 0.5*h*k1x*k0u;
                     k2u = k2u + 0.5*h*k2x*k1u;
                     k3u = k3u + h*k3x*k2u;
-
                     % Chain Rule Correction (State) | diff_x(x_k + 0.5*h*k0) = 
                     % = eye() + 0.5*h*k0x.
                     k1x = k1x*(eye(obj.nx) + 0.5*h*k0x);
                     k2x = k2x*(eye(obj.nx) + 0.5*h*k1x);
                     k3x = k3x*(eye(obj.nx) + h*k2x);
-
                     % Rewrite Gradients
                     fx = eye(obj.nx) + (h/6)*(k0x + 2*k1x + 2*k2x + k3x);
                     fu = (h/6)*(k0u + 2*k1u + 2*k2u + k3u);
-
                 %% Implicit Time Integration
                 case "ode1i"
                     % Compute Analytical Jacobian
@@ -203,13 +213,11 @@ classdef GVS_Dynamics < Dynamics
                     [~, gx_next, gx, gu] = computeResidualAndJacobian(t, x_new, x, u, h, obj, "method", options.method);
                     fx = - gx_next\gx;
                     fu = - gx_next\gu;
-
                 otherwise
                     error("Unsupported Discretization Method.");
             end
-
         end
-
+        
         %% Numerical Hessian
         function [fxx, fxu, fuu] = numerical_hessian(obj, x, u, dt, options)
             arguments
@@ -224,47 +232,40 @@ classdef GVS_Dynamics < Dynamics
             fxx = zeros(obj.nx, obj.nx, obj.nx);
             fxu = zeros(obj.nx, obj.nx, obj.nact);
             fuu = zeros(obj.nx, obj.nact, obj.nact);
-
             % Storing Canonical Base
             x_bases = eye(obj.nx);
             u_bases = eye(obj.nact);
-
-            % Differentiation w.r.t. x
+            % Differentiating w.r.t. x
             for i = 1:obj.nx
-                % Perturbating x
+                % Perturbing x
                 x_tilde1 = x - options.eps*x_bases(:, i);
                 x_tilde2 = x + options.eps*x_bases(:, i);
-
                 % Compute Analytical Derivatives
                 [fx1, ~] = obj.discretized_gradients(0, x_tilde1, obj.dynamics(0, x_tilde1, u), u, dt);
                 [fx2, ~] = obj.discretized_gradients(0, x_tilde2, obj.dynamics(0, x_tilde2, u), u, dt);
-
                 % Finite Difference
                 fxx(:, :, i) = 0.5*(fx2 - fx1)/options.eps;
             end
-
-            % Differentiation w.r.t. u
+            % Differentiating w.r.t. u
             for i = 1:obj.nact
-                % Perturbating x
+                % Perturbing u
                 u_tilde1 = u - options.eps*u_bases(:, i);
                 u_tilde2 = u + options.eps*u_bases(:, i);
-
                 % Compute Analytical Derivatives
                 [fx1, fu1] = obj.discretized_gradients(0, x, obj.dynamics(0, x, u_tilde1), u_tilde1, dt);
                 [fx2, fu2] = obj.discretized_gradients(0, x, obj.dynamics(0, x, u_tilde2), u_tilde2, dt);
-
                 % Finite Difference
                 fxu(:, :, i) = 0.5*(fx2 - fx1)/options.eps;
                 fuu(:, :, i) = 0.5*(fu2 - fu1)/options.eps;
             end
-
             %% Symmetrize
             fxx = 0.5*(fxx + permute(fxx, [1 3 2]));
             fuu = 0.5*(fuu + permute(fuu, [1 3 2]));
         end
-
     end
 end
+
+%% Helper Functions (Outside the Classdef)
 
 function [F, Jx_next, Jx, Ju] = computeResidualAndJacobian(t, x_next, x_current, u, h, dyn_obj, options)
     arguments
@@ -276,20 +277,16 @@ function [F, Jx_next, Jx, Ju] = computeResidualAndJacobian(t, x_next, x_current,
         dyn_obj
         options.method = "ode1i"     % obj is the object containing your dynamics and derivatives methods
     end
-
     % f(x_next, u)
     x_next_dot = dyn_obj.dynamics(t, x_next, u);
-
     % Compute only if fsolve requires Jacobian
     if nargout > 1
         [fx_next, fu_next] = dyn_obj.analytical_derivatives(t, x_next, x_next_dot, u);
     end
-
     switch(options.method)
         case "ode1i"
             % 1. Compute Residual
             F = x_next - x_current - h * x_next_dot;
-
             if nargout > 1
                 % 2. Compute the Jacobian of the residual
                 n = numel(x_current); % Get the size of the state vector
@@ -299,17 +296,13 @@ function [F, Jx_next, Jx, Ju] = computeResidualAndJacobian(t, x_next, x_current,
                 Jx = - I;
                 Ju = - h * fu_next;
             end
-
         case "trapz"
             % f(x_current, u)
             x_current_dot = dyn_obj.dynamics(t, x_current, u);
-
             % 1. Compute Residual
             F = x_next - x_current - 0.5*h*(x_next_dot + x_current_dot);
-
             if nargout > 1
                 [fx_current, fu_current] = dyn_obj.analytical_derivatives(t, x_current, x_current_dot, u);
-
                 % 2. Compute the Jacobian of the residual
                 n = numel(x_current); % Get the size of the state vector
                 I = eye(n);
@@ -318,7 +311,6 @@ function [F, Jx_next, Jx, Ju] = computeResidualAndJacobian(t, x_next, x_current,
                 Jx = - I - 0.5*h*fx_current;
                 Ju = - 0.5*h*(fu_current + fu_next);
             end
-
         otherwise
             error("Unsupported Implicit Integration Method.")
     end
